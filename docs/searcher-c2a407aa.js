755a36b8add6b8455ec17a6d9571608aa45625e4
hansnone <decabopedro@gmail.com>
2025-12-01 16:11:24 +0100

    commit

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f60f4926.js',
+                path_to_root + 'searchindex-6f30cc24.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
